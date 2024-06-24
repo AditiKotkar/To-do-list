0608d141f8ee395e7df4f710b2d8b03a61ff81d7
AditiKotkar <150508095+AditiKotkar@users.noreply.github.com>
2024-06-24 13:01:46 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 window.addEventListener('load', () => {
-	todos = JSON.parse(localStorage.getItem('todos')) || [];
+	let todos = JSON.parse(localStorage.getItem('todos')) || [];
 	const nameInput = document.querySelector('#name');
 	const newTodoForm = document.querySelector('#new-todo-form');
 
@@ -9,7 +9,7 @@ window.addEventListener('load', () => {
 
 	nameInput.addEventListener('change', (e) => {
 		localStorage.setItem('username', e.target.value);
-	})
+	});
 
 	newTodoForm.addEventListener('submit', e => {
 		e.preventDefault();
@@ -19,7 +19,7 @@ window.addEventListener('load', () => {
 			category: e.target.elements.category.value,
 			done: false,
 			createdAt: new Date().getTime()
-		}
+		};
 
 		todos.push(todo);
 
@@ -28,15 +28,17 @@ window.addEventListener('load', () => {
 		// Reset the form
 		e.target.reset();
 
-		DisplayTodos()
-	})
+		DisplayTodos();
+	});
 
-	DisplayTodos()
-})
+	DisplayTodos();
+});
 
-function DisplayTodos () {
+function DisplayTodos() {
 	const todoList = document.querySelector('#todo-list');
 	todoList.innerHTML = "";
+
+	const todos = JSON.parse(localStorage.getItem('todos')) || [];
 
 	todos.forEach(todo => {
 		const todoItem = document.createElement('div');
@@ -63,7 +65,7 @@ function DisplayTodos () {
 		edit.classList.add('edit');
 		deleteButton.classList.add('delete');
 
-		content.innerHTML = <input type="text" value="${todo.content}" readonly> ;</input>
+		content.innerHTML = `<input type="text" value="${todo.content}" readonly />`;
 		edit.innerHTML = 'Edit';
 		deleteButton.innerHTML = 'Delete';
 
@@ -80,7 +82,7 @@ function DisplayTodos () {
 		if (todo.done) {
 			todoItem.classList.add('done');
 		}
-		
+
 		input.addEventListener('change', (e) => {
 			todo.done = e.target.checked;
 			localStorage.setItem('todos', JSON.stringify(todos));
@@ -91,9 +93,8 @@ function DisplayTodos () {
 				todoItem.classList.remove('done');
 			}
 
-			DisplayTodos()
-
-		})
+			DisplayTodos();
+		});
 
 		edit.addEventListener('click', (e) => {
 			const input = content.querySelector('input');
@@ -103,16 +104,14 @@ function DisplayTodos () {
 				input.setAttribute('readonly', true);
 				todo.content = e.target.value;
 				localStorage.setItem('todos', JSON.stringify(todos));
-				DisplayTodos()
-
-			})
-		})
+				DisplayTodos();
+			});
+		});
 
 		deleteButton.addEventListener('click', (e) => {
 			todos = todos.filter(t => t != todo);
 			localStorage.setItem('todos', JSON.stringify(todos));
-			DisplayTodos()
-		})
-
-	})
+			DisplayTodos();
+		});
+	});
 }
